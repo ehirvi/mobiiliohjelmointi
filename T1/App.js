9538f14f6a16228d0785620a0250d288596a556e
ehirvi <eino.hirvi@gmail.com>
2024-08-23 12:42:49 +0300

    Tehtävä 1 (Laskin)

---
+++ T1/App.js
@@ -0,0 +1,65 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+const App = () => {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const calcResult = (expression) => {
+    if (expression === "+") {
+      setResult(value1 + value2);
+    } else if (expression === "-") {
+      setResult(value1 - value2);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Result: {result}</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={value1}
+        onChangeText={(val) => setValue1(Number(val))}
+      />
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={value2}
+        onChangeText={(val) => setValue2(Number(val))}
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          width: 80,
+          margin: 10,
+        }}
+      >
+        <Button title=" + " onPress={() => calcResult("+")}></Button>
+        <Button title=" - " onPress={() => calcResult("-")}></Button>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 150,
+    borderWidth: 1,
+    margin: 4,
+    padding: 4,
+  },
+});
+
+export default App;
